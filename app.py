ba3494098eba3cac2599207e0ab9c11bd9be452b
Kauan <jailma.kmp@gmail.com>
2022-07-28 23:37:42 -0300

    [feat] show pokemons names at index file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Imports
 from flask import Flask, render_template
-
+import json
+import os
 
 # Flask
 app = Flask(__name__)
@@ -8,5 +9,12 @@ app = Flask(__name__)
 
 #Routes
 @app.route("/")
-def main():
-    return render_template("index.html")
+def index():
+    names = []
+    files = os.listdir('pokeinfo/pokemons')     # array with the names of the pokemon files
+    for archive in files:
+        with open(f'pokeinfo/pokemons/{archive}') as f:
+            pokemon = json.load(f)
+            names.append(pokemon["name"])       # for each pokemon add its name to 'names' array
+
+    return render_template("index.html", data=names)
